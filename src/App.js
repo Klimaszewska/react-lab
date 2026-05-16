7c62b063b9617b1ca80289bf2690ff6cab6441e8
Klimaszewska <klimaszewska.justyna@gmail.com>
2026-05-16 11:04:25 +0200

    add react hooks for email data and validation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'user@example.com';
+    let [email, setEmail] = useState('user@example.com');
+    let [validation, setValidation] = useState('');
+
+    function validateEmailLength(emailInput) {
+        if (emailInput.length < 5) {
+            setValidation("Ale masz krótki adres!")
+        } else if (emailInput.length > 5 && emailInput.length < 35) {
+            setValidation("Email jest dobry!")
+        } else {
+            setValidation("Email jest za długi!")
+        }
+    }
 
     function handleChange(event) {
-        console.log(event.target.value);
+        let emailInput = event.target.value;
+        setEmail(emailInput);
+        validateEmailLength(emailInput);
     }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
             <h2>Twój e-mail to: {email}</h2>
-            <input type="text" placeholder="Wpisz swój e-mail" onChange={handleChange} />
+            <div>{validation}</div>
+            <input type="text" value={email} onChange={handleChange} />
         </div>
     );
 }
